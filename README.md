c03e0a77f8dfd2b235c363ceb95897c37d633075
Marcin Wasilewski <kasandrop@gmail.com>
2023-11-25 11:46:22 +0000

    init spring boot

---
+++ README.md
@@ -0,0 +1,22 @@
+# todo-api
+TO MANAGE DATABASE go to 
+
+localhost:8091/h2-console
+
+username:MARCIN
+pass:MARCIN1
+
+to run a backend :
+mvn compile exec:java
+
+to stop the service: 
+Ctrl+C
+
+requirements:
+1.   java version 17
+2.  Apache Maven 3.3 or above
+3. 
+
+
+
+
---
+++ src/main/java/com/marcin/todo/TodoApplication.java
@@ -0,0 +1,15 @@
+package com.marcin.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class TodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(com.marcin.todo.TodoApplication.class, args);
+    }
+
+}
